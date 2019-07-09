3f53239e2969e46ce96d980e806882d8d57ae557
ChristineWasike <christinewasike22@gmail.com>
2019-07-09 14:53:13 +0300

    Can now add deseired food the the fodlist Arrays, add methods and on click listeners were involved

--- app/src/main/java/com/spongey/dinnerdecider/MainActivity.kt
+++ app/src/main/java/com/spongey/dinnerdecider/MainActivity.kt
@@ -8,7 +8,7 @@ import java.util.*
 @Suppress("PLUGIN_WARNING")
 class MainActivity : AppCompatActivity() {
 
-    private val foodList = arrayListOf("Rice", "Ug../ali", "KFC Streetwise 2", "Fish & Chips")
+    private val foodList = arrayListOf("Rice", "Ugali", "KFC Streetwise 2", "Fish & Chips")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,6 +18,13 @@ class MainActivity : AppCompatActivity() {
             val random = Random()
             val randomFood = random.nextInt(foodList.count())
             foodType.text = foodList[randomFood]
+        }
+
+        addFoodButton.setOnClickListener {
+            val newFood = newFoodText.text.toString()
+            foodList.add(newFood)
+            newFoodText.text.clear()
+            println(foodList)
         }
     }
 }
