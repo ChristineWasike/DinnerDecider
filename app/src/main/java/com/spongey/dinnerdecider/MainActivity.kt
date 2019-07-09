2ff119ceb3ae517aa92d4ce2f2ef2d2219cc96a1
ChristineWasike <christinewasike22@gmail.com>
2019-07-09 14:47:07 +0300

    Food type changing when deciding what to it Implemented the random number genrator to call the indices in the food list array

--- app/src/main/java/com/spongey/dinnerdecider/MainActivity.kt
+++ app/src/main/java/com/spongey/dinnerdecider/MainActivity.kt
@@ -3,13 +3,21 @@ package com.spongey.dinnerdecider
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
+@Suppress("PLUGIN_WARNING")
 class MainActivity : AppCompatActivity() {
+
+    private val foodList = arrayListOf("Rice", "Ug../ali", "KFC Streetwise 2", "Fish & Chips")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        foodType.text = "Burger King"
+        decideButton.setOnClickListener {
+            val random = Random()
+            val randomFood = random.nextInt(foodList.count())
+            foodType.text = foodList[randomFood]
+        }
     }
 }
