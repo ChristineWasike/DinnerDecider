1dfc2f9f83b822b6fd46f87fd06d9a610d5d921d
ChristineWasike <christinewasike22@gmail.com>
2019-07-26 10:31:36 +0300

    Added .idea to .gitignore to ignore entries

---
+++ app/src/main/java/com/spongey/dinnerdecider/Testing.kt
@@ -0,0 +1,12 @@
+package com.spongey.dinnerdecider
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Testing : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_testing)
+    }
+}
